3ac8751aad2ff273dc375824eb0c9a062d3daeb4
mikhail-bn <mihail.news@gmail.com>
2023-01-30 14:17:40 +0200

    30-01-23

--- logic_operators.js
+++ logic_operators.js
@@ -26,6 +26,46 @@ const userAge = 55;
 //     console.log('See you next time...');
 // }
 
+// const currentOurs = 7;
+// if(currentOurs < 8 || currentOurs > 20) {
+//     console.log('Приходите завтра');
+// } else {
+//     console.log('Welcome!'); 
+// }
+
+// const userNickname = 'Mikhail';
+// const defaultNickname = '';
+// const nickname = userNickname || defaultNickname || 'noname'; // При логическом ИЛИ || получая значение false идет к следующей переменной, получая true - присваивает значение
+// console.log('nickname', nickname);
+
+// const finalNickname = userNickname && 'user exists'; // При логическом И && всё наоборот. Получая false - присваивает значение, получая true переходит к следующей переменной.
+
+// console.log('finalNickname', finalNickname);
+
+// ! логическое НЕ
+
+// const hasAccess = true;
+// if(!hasAccess) {
+//     console.log('Access denied!');
+// } else {
+//     console.log('Access allowed');
+// };
+
+// const answer = prompt('How old are you?');
+// if(!answer){
+//     alert('Please, write your age.');
+// } else {
+//     alert(`You are ${answer} years old`)
+// }
+
+// Оператор объединения с null   ??
+
+// При операторе || ложными будут значения false 0 '' undefined null NaN
+console.log(false || 'Hello world');
+// При операторе ?? ложными будут значения только undefined null, то есть получив запись false считает его как true и выводит в консоль
+console.log(false ?? 'Hello world');
+
+
 
 
 
